d10f560b6f826b215be1c3d847ca1c082b531a4b
EmilioGarzaC <emilio@valiot.io>
2023-05-28 13:12:59 -0500

    Added project structure and first test

---
+++ src/__main__.py
@@ -0,0 +1,6 @@
+
+def runProgram():
+    if __name__ == '__main__':
+        return 'hello'
+        
+runProgram()
---
+++ tests/test_RegisterNewExpense.py
@@ -0,0 +1,4 @@
+import __main__ as main
+
+def test_ReturnsHello():
+    assert main.runProgram() == 'hello'
