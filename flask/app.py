d9571a32ceade01505fdd661913e4c16b0109378
EmilioGarzaC <emilio@valiot.io>
2023-06-01 22:24:37 -0500

    Added popup form and connected it to the backend

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('charts.html')
+
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    amount = request.form.get('amount')
+    description = request.form.get('description')
+    date = request.form.get('date')
+
+    return [amount, description, date]
