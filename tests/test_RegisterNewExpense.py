8d4c2534a414ad217b364c662db6ab11e5194de7
EmilioGarzaC <emilio@valiot.io>
2023-07-04 16:43:34 -0500

    added gunicorn to requirements

--- tests/test_RegisterNewExpense.py
+++ tests/test_RegisterNewExpense.py
@@ -4,3 +4,5 @@ import modules.movementManagers.expenseManager as expenseManager
 def test_ReturnsHello():
     expMan = expenseManager.ExpenseManager()
     assert expMan.test_func() == 1
+    
+    #gunicorn --bind=0.0.0.0 --timeout 600 flask/app:app
